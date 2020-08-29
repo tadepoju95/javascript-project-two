f232e331a37afd68562ec3df8aa29eb3bfe54c33
Taiwo Adepoju <tadepoju95@gmail.com>
2020-08-29 07:07:08 -0500

    Modified all files in the project folder.

---
+++ colorswitcher.js
@@ -0,0 +1,21 @@
+/*
+var pickColor = () => {
+	let optionsOfColor = document.getElementById('colors').value; 
+	if(optionsOfColor === 'brown') {
+		return document.getElementById('colorOptions').style.backgroundColor = 'brown';
+	}
+}
+*/
+
+var pickColor = () => {
+	let optionsOfColor = document.getElementById('colors').value; 
+    console.log(optionsOfColor);
+	if(optionsOfColor === 'black') {
+		return document.getElementById('colorOptions').style.backgroundColor = 'black';
+	}
+}
+
+
+
+
+
