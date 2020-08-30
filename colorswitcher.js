db3801d1f46405d52cb98778055d391b4d0b113f
Taiwo Adepoju <tadepoju95@gmail.com>
2020-08-30 01:57:47 -0500

    Modified all files in project folder.

--- colorswitcher.js
+++ colorswitcher.js
@@ -1,17 +1,16 @@
-/*
-var pickColor = () => {
-	let optionsOfColor = document.getElementById('colors').value; 
-	if(optionsOfColor === 'brown') {
-		return document.getElementById('colorOptions').style.backgroundColor = 'brown';
-	}
-}
-*/
-
-var pickColor = () => {
+let pickColor = () => {
 	let optionsOfColor = document.getElementById('colors').value; 
     console.log(optionsOfColor);
-	if(optionsOfColor === 'black') {
+	if(optionsOfColor === 'pink') {
+		return document.getElementById('colorOptions').style.backgroundColor = 'pink';
+	} else if(optionsOfColor === 'black') {
 		return document.getElementById('colorOptions').style.backgroundColor = 'black';
+	} else if(optionsOfColor === 'brown') {
+		return document.getElementById('colorOptions').style.backgroundColor = 'brown';
+	} else if(optionsOfColor === 'blue') {
+		return document.getElementById('colorOptions').style.backgroundColor = 'blue';
+	} else if(optionsOfColor === 'orange') {
+		return document.getElementById('colorOptions').style.backgroundColor = 'orange';
 	}
 }
 
